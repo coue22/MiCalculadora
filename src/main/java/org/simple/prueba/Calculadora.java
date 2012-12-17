f804d715731a93ee0ea388a99ddc7f696d161c75
coue22 <coue22@gmail.com>
2012-12-17 12:31:45 +0100

    Commit Inicial

---
+++ src/main/java/org/simple/prueba/Calculadora.java
@@ -0,0 +1,93 @@
+package org.simple.prueba;
+
+
+public class Calculadora 
+{
+
+	/**
+	 * Constructor
+	 */
+	public Calculadora(){
+	}
+	 
+	/**
+	 * Realiza una suma
+	 * @param a - primer sumando
+	 * @param b - segundo sumando
+	 * @return - La suma de los dos sumandos
+	 */
+	public int sumar(int a, int b){
+		return a + b;
+	}
+
+	/**
+	 * Realiza una resta
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int restar(int a, int b){
+		return a - b;
+	}
+	
+	/**
+	 * Realiza la multiplicación.
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int multiplicar(int a, int b){
+		return a * b;
+	}
+	
+	/**
+	 * Realiza la división.
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int dividir(int a, int b){
+		if (b == 0){
+			return 0;
+		}
+			
+		
+		return a / b;
+	}
+	
+	/**
+	 * Realiza la raiz cuadrada
+	 * @param a - Numero del cual se quiere realizar la operación.
+	 * @return
+	 */
+	public double raiz(double a){
+		return Math.sqrt(a); 
+	}
+	
+	/**
+	 * 
+	 * @param a
+	 * @return
+	 */
+	public double potencia(double a){
+		return Math.pow(a,2); 
+	}
+		
+
+	
+	/**
+	 * 
+	 * @param a
+	 * @return
+	 */
+	public double Logaritmo(double a){
+		if (a==0)
+			return 1;
+		
+		return 2;
+	}
+			
+	
+}
+
+
---
+++ src/test/java/org/simple/prueba/AppTest.java
@@ -0,0 +1,67 @@
+package org.simple.prueba;
+
+import org.junit.Test;
+
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+
+	private Calculadora calc;
+	
+	
+	@Test
+    public void testSuma()
+    {
+		calc = new Calculadora();
+		System.out.println("Salida de la Suma: " + calc.sumar(5, 7));
+    }
+
+	
+	@Test
+    public void testResta()
+    {
+		calc = new Calculadora();
+		System.out.println("Salida de la Resta: " + calc.restar(9, 7));
+    }
+	
+	@Test
+    public void testMulti()
+    {
+		calc = new Calculadora();
+		System.out.println("Salida de la Multiplicacion: " + calc.multiplicar(9, 7));
+    }	
+
+	@Test
+    public void testDividir()
+    {
+		calc = new Calculadora();
+		System.out.println("Salida de la Division: " + calc.dividir(9, 3));
+    }	
+
+	@Test
+    public void testRaiz()
+    {
+		calc = new Calculadora();
+		System.out.println("Salida de la Raiz: " + calc.raiz(8));
+    }	
+	
+	@Test
+    public void testPotencia()
+    {
+		calc = new Calculadora();
+		System.out.println("Salida de la Potencia: " + calc.potencia(8));
+    }	
+		
+	@Test
+    public void testLogaritmo()
+    {
+		calc = new Calculadora();
+		System.out.println("Salida de la Logaritmo: " + calc.Logaritmo(8));
+    }	
+		
+}
